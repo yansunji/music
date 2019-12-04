bd0d719cede74c8aa867dddc15741ff715d5d6f0
yansj <yansj@citycloud.com.cn>
2019-12-04 13:45:18 +0800

    单点登录

---
+++ sso/src/test/java/com/citycloud/music/sso/SsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citycloud.music.sso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
