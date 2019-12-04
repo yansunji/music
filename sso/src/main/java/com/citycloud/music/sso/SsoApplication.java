fd5d6a00869657df741b32b4745e8e4fb1e95698
yansj <yansj@citycloud.com.cn>
2019-12-04 14:36:49 +0800

    单点登录

--- sso/src/main/java/com/citycloud/music/sso/SsoApplication.java
+++ sso/src/main/java/com/citycloud/music/sso/SsoApplication.java
@@ -3,11 +3,14 @@ package com.citycloud.music.sso;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author: Johnson<YSJ>
+ * @date: 2019/12/4 13:48
+ * @description: 单点登录启动类
+ */
 @SpringBootApplication
 public class SsoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SsoApplication.class, args);
     }
-
 }
